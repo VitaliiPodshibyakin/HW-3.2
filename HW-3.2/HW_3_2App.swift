8c4c0f9c8157610380a75d4ab76a0adfa7db9709
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-05-10 20:56:49 +0300

    Initial Commit

---
+++ HW-3.2/HW_3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_2App.swift
+//  HW-3.2
+//
+//  Created by Виталий Подшибякин on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
